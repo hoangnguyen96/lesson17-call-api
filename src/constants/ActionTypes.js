7fbff20d82a0f8228f80d697a03cd7bbb55afa9f
nguyen-vanhoang <nguyen-vanhoang@arch-vietnam.vn>
2018-09-05 09:12:19 +0700

    Finish project

---
+++ src/constants/ActionTypes.js
@@ -0,0 +1,5 @@
+export const FETCH_PRODUCTS = 'FETCH_PRODUCTS'; //lay all products
+export const ADD_PRODUCT = 'ADD_PRODUCT';
+export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
+export const DELETE_PRODUCT = 'DELETE_PRODUCT';
+export const EDIT_PRODUCT = 'EDIT_PRODUCT';
